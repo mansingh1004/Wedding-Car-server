a3f6cae38a7a91ab7619b8c60c0ca0528f39ac6e
Man Singh <156889187+mansingh1004@users.noreply.github.com>
2026-02-17 11:01:09 +0530

    gmail

--- controllers/inquiryController.js
+++ controllers/inquiryController.js
@@ -137,17 +137,24 @@ const createInquiry = async (req, res) => {
 
 
 
+// const transporter = nodemailer.createTransport({
+//   host: 'smtp.gmail.com',
+//   port: 465,
+//   secure: true, // true for 465, false for other  ports  
+//   auth: {
+//     user: process.env.EMAIL_USER, 
+//     pass: process.env.EMAIL_PASS  
+//   }
+// });
+
+
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true, // true for 465, false for other  ports  
+  service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER, 
-    pass: process.env.EMAIL_PASS  
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
   }
 });
-
-
 
     
 
